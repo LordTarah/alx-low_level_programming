0f15cf6ff8b8729e0d4eee802cf9608d742c0238
LordTarah <joeltarah0@gmail.com>
2023-03-25 18:02:35 +0400

    hope

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 0; n < 10; n++)
 	{
-		putchar(n);
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-return (0);
+		if (n == 9)
+			putchar(n + '0');
+		else
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
 }
