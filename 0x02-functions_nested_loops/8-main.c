28906978fac40300e54a1c9afa176df3905f6b70
LordTarah <joeltarah0@gmail.com>
2023-03-29 00:34:59 +0400

    Right behind you

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ *jack_bauer: Variable whose time is being measured.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
