e7e32f6ea0dc7665f4e8b89a004132ece06b1ecf
LordTarah <joeltarah0@gmail.com>
2023-04-10 19:50:54 +0400

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *_strncat - concentrates two strings
+  *@src: The string appended to dest
+  * an inputed number of bytes from src
+  *@dest:The string to be appended
+  *@n: Number of bytes fr0m src
+  *return: Pointer to existing string
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
